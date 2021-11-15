13a40f4fa60a2f941a997107a6f9c2dbb07a34b4
alexd <alex.degouzon@etu.univ-nantes.fr>
2021-11-15 14:32:03 +0100

    création de la fenetre

--- Game.py
+++ Game.py
@@ -7,17 +7,20 @@ def game_loop(window):
 
     quitting = False
     game_state = GameState()
+    game_state.draw(window)
     while not quitting:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitting = True
+        pygame.display.update()
+
 
 def main():
     pygame.init()
     GameConfig.init()
     window = pygame.display.set_mode((GameConfig.WINDOW_L, GameConfig.WINDOW_H))
     pygame.display.set_caption("VR de Terre.exe")
-    game_loop()
+    game_loop(window)
     pygame.quit()
     quit()
 
