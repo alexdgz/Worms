18396e9a2febd1e1eb9e389d139a6253f1dc60df
Lucas <lucas.maillet44@gmail.com>
2021-12-03 22:02:29 +0100

    new projecile

--- Shoot.py
+++ Shoot.py
@@ -26,10 +26,10 @@ class Shoot(pygame.sprite.Sprite):
             return True
         return False
 
-    def fg(self,y):#
+    def fg(self,y):# calcule de f pour euler
         return np.array([0,GameConfig.GRAVITY/GameConfig.MASSE,y[0],y[1]])
 
-    def euler(self,y,dt,f):
+    def euler(self,y,dt,f): # calcule de euler
         return y + dt * f(y)
 
 
@@ -40,8 +40,10 @@ class Shoot(pygame.sprite.Sprite):
                self.draw(window)
                y = np.array([GameConfig.VECTEUR.x, GameConfig.VECTEUR.y, self.rect.left, self.rect.top])
                Y = self.euler(y,GameConfig.DT,self.fg)
+                # redéfinition des vecteur avec les nouveaux calculés
                GameConfig.VECTEUR.x = Y[0]
                GameConfig.VECTEUR.y = Y[1]
+               #redefinition de la position du projectile
                self.rect = self.rect.move(Y[2]-self.rect.left,Y[3]-self.rect.top)
                self.rect.left = Y[2]
                self.rect.top = Y[3]
